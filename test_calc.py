b9d518dede8c9cc39f883f651ad3989058dcfc9b
Pavel Zaikin <pzaikin@cloudlinux.com>
2020-02-12 12:41:30 +0500

    unrussian

--- test_calc.py
+++ test_calc.py
@@ -9,12 +9,4 @@ import calculator as calc
     [0.5, 0.5, 1]
 ])
 def test_add(a, b, result):
-    """ Функция calc.add сложение двух чисел
-
-        1 + 2 = 3
-        -1 + 1 = 0
-        0.5 + 0.5 = 1
-        1/3 + 1/3 = 2/3
-
-    """
     assert calc.add(a, b) == result
