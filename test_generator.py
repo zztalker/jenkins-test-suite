688725c855139fff90a30b97248ddad8cb4a115f
Pavel Zaikin <pzaikin@cloudlinux.com>
2022-02-17 15:53:18 +0500

    reduce amount of tests

--- test_generator.py
+++ test_generator.py
@@ -3,9 +3,9 @@ import time
 import pytest
 
 
-@pytest.mark.parametrize('a', [i for i in range(10)])
-@pytest.mark.parametrize('b', [i for i in range(10)])
-@pytest.mark.parametrize('c', [i for i in range(10)])
+@pytest.mark.parametrize('a', [i for i in range(3)])
+@pytest.mark.parametrize('b', [i for i in range(3)])
+@pytest.mark.parametrize('c', [i for i in range(3)])
 def test_add(a, b, c):
-    time.sleep(random.random())
+    time.sleep(random.random()/100)
     assert random.random() <= 0.9
